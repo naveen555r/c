9b3b5da04802121413380a40846879b3faeaef45
naveen555r <naveen555ravi@gmail.com>
2024-03-05 13:28:05 +0530

    first commit

--- string.c
+++ string.c
@@ -17,12 +17,33 @@
 //     printf("%d",strlen (alphabet));
 //     return 0;
 // }
+// #include<stdio.h>
+// #include<string.h>
+// int main(){
+//     char str1[20]="good";
+//     char str2[]="morning";
+//     strcat(str1,str2);
+//     printf("%s",str1);
+//     return 0;
+// }
+// #include<stdio.h>
+// #include<string.h>
+// int main(){
+//     char str1[20]="good";
+//     char str2[]="morning";
+//     strcat(str1,str2);
+//     printf("%s",str1);
+//     return 0;
+// }
 #include<stdio.h>
-#include<string.h>
 int main(){
-    char str1[20]="good";
-    char str2[]="morning";
-    strcat(str1,str2);
-    printf("%s",str1);
+    int arr[]={1,2,3,4,5,6};
+    int n=sizeof(arr)/sizeof (arr[0]);
+    int total=0;
+    for(int i=0;i<n;i++){
+    total+=arr[i];
+    }
+    printf("sum of array elements:%d\n",total);
     return 0;
-}
+}         
+
